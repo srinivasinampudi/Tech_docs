44fe901513f30c01f269a65c531f3b39b2c755da
srinivasinampudi <60294328+srinivasinampudi@users.noreply.github.com>
2021-02-23 16:45:33 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git & Github Documentation'
+copyright = '2021, Srinivas'
+author = 'Srinivas'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
